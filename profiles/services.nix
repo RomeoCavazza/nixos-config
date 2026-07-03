d379b4087a089afdf648220bfe10300d21ae0df2
RomeoCavazza <romeo.cavazza@gmail.com>
2026-07-03 05:34:25 +0200

    Remove global database services

--- profiles/services.nix
+++ profiles/services.nix
@@ -2,7 +2,6 @@
   imports = [
     ../modules/services/backup.nix
     ../modules/services/guix.nix
-    ../modules/services/databases.nix
     ../modules/services/nginx.nix
     ../modules/services/ollama.nix
     ../modules/services/virtualisation.nix
