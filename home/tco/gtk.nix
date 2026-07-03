8b0fb705c01b13288f60c781425582e13aef4d8d
RomeoCavazza <romeo.cavazza@gmail.com>
2026-07-03 20:26:39 +0200

    Tame Nemo GTK colors under Stylix

--- home/tco/gtk.nix
+++ home/tco/gtk.nix
@@ -1,7 +1,42 @@
-{ pkgs, ... }:
+{ pkgs, theme, ... }:
+
+let
+  inherit (theme) palette;
+in
 
 {
-  stylix.targets.gtk.enable = true;
+  stylix.targets.gtk = {
+    enable = true;
+    extraCss = ''
+      /* Nemo is GTK3/Cinnamon and can over-amplify generated theme tints. */
+      .nemo-window .view,
+      .nemo-window iconview,
+      .nemo-window treeview,
+      .nemo-window .places-treeview {
+        background-color: ${palette.base};
+        color: ${palette.text};
+      }
+
+      .nemo-window .places-treeview:selected,
+      .nemo-window treeview:selected,
+      .nemo-window .view:selected,
+      .nemo-window iconview:selected {
+        background-color: alpha(${palette.accent}, 0.16);
+        color: ${palette.text};
+      }
+
+      .nemo-window .nemo-inactive-pane .view:not(:selected),
+      .nemo-window .nemo-inactive-pane iconview {
+        background-color: ${palette.mantle};
+      }
+
+      .nemo-window .floating-bar {
+        background-color: ${palette.surface0};
+        color: ${palette.text};
+        border-color: ${palette.surface1};
+      }
+    '';
+  };
 
   gtk = {
     enable = true;
