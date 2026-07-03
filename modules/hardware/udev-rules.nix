787dc8f1e46f8739cc591a738883252adfeb42b8
RomeoCavazza <romeo.cavazza@gmail.com>
2026-07-03 23:23:23 +0200

    chore(repo): add MIT LICENSE + pin GitHub Actions to commit SHA (supply-chain)

--- modules/hardware/udev-rules.nix
+++ modules/hardware/udev-rules.nix
@@ -2,9 +2,9 @@ _:
 
 {
   services.udev.extraRules = ''
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="04b5", ATTRS{idProduct}=="6cde", MODE="0666", TAG+="uaccess"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="04b5", ATTRS{idProduct}=="6cde", TAG+="uaccess"
 
-    KERNEL=="ttyACM*", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="5740", MODE="0666", TAG+="uaccess"
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="5740", MODE="0666", TAG+="uaccess"
+    KERNEL=="ttyACM*", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="5740", TAG+="uaccess"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="5740", TAG+="uaccess"
   '';
 }
