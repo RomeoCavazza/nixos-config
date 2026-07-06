40e5c5013a3ba46e208fafeb04765be3cb04e635
RomeoCavazza <romeo.cavazza@gmail.com>
2026-07-07 00:10:31 +0200

    fix(nginx): forward original host with port for gitlab

--- modules/services/nginx.nix
+++ modules/services/nginx.nix
@@ -93,8 +93,8 @@ in
             proxyPass = gitlabSocket;
             proxyWebsockets = true;
             extraConfig = ''
-              proxy_set_header Host              gitlab.localhost;
-              proxy_set_header X-Forwarded-Host  gitlab.localhost;
+              proxy_set_header Host              $http_host;
+              proxy_set_header X-Forwarded-Host  $http_host;
               proxy_set_header X-Real-IP         $remote_addr;
               proxy_set_header X-Forwarded-For   $proxy_add_x_forwarded_for;
               proxy_set_header X-Forwarded-Proto $scheme;
